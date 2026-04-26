d9818d29020885c8b6158211e295e8c3ac9772e6
jsudoRc <julio.src2006@gmail.com>
2026-04-26 12:39:28 -0300

    Arrays paralelas criadas

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+//Defining the core arrays 
+
+const titulos : string [] = [];
+const autores : string [] = [];
+
+const lido : boolean [] = [];
+
+const anos : number [] = [];
+const paginas : number [] = [];
+const avaliacoes: number [] = [];
+
+titulos.push(
+    'Harry Potter e a Pedra Filosofal',
+    'Percy Jackson e o Mar de Monstros',
+    'O Algebrista',
+    'Matemática Elementar',
+    'Harry Potter e a câmara secreta',
+    'Percy Jackson e o Ladrão de Raios'
+)
+
